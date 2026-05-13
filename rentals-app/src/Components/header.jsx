01216e4f2b26fb0777bf3f978b57b01409b716a1
Finley <fynmwc@gmail.com>
2026-05-14 09:04:30 +1000

    setup router

---
+++ rentals-app/src/Components/header.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Pages/Home";
+import Search from "./Pages/Search";
+import Login from "./Pages/Login";
+import Rentals from "./Pages/Rentals";
+
+
+function AppLayout() {
+  return (
+    <div className="App">
+      <Header />
+      <main style={{ padding: "1rem" }}>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: AppLayout,
+    children: [
+      { index: true, Component: Home },
+      { path: "search", Component: Search },
+      { path: "rentals", Component: Rentals },
+      { path: "login", Component: Login },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
